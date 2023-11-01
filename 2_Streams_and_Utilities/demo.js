dcca3648646e2d06102937529655d1ed984555fb
Mishakaveli1994 <mihata1994@yahoo.com>
2023-11-01 23:44:01 +0200

    Updates

--- 2_Streams_and_Utilities/demo.js
+++ 2_Streams_and_Utilities/demo.js
@@ -1,7 +1,29 @@
 const fs = require('fs')
 
 const readStream = fs.createReadStream('./views/cats.html', 'utf-8')
+const writeStream = fs.createWriteStream('./write.txt', { encoding: 'utf8' })
+
+let sequenceCounter = 0
+
+function incrementCounter() {
+  sequenceCounter++
+  return sequenceCounter
+}
 
 readStream.on('data', (data) => {
-  console.log(data)
+  console.log(`${incrementCounter()}. Starting read stream`)
+  writeStream.write(data)
+  console.log(`${incrementCounter()}. Writing to write stream`)
+  writeStream.write(data)
 })
+
+readStream.on('end', () => {
+  console.log(`${incrementCounter()}. Finished reading`)
+  writeStream.end() // It's here as the read and write are both asynchronous
+})
+
+writeStream.on('finish', () => {
+  console.log(`${incrementCounter()}. Finished writing`)
+})
+
+console.log(`${incrementCounter()}. This is at the end of the file`)
