8eda1fff0e1908b5170755422d40bf959d4ddf5b
mtopuzov <mtopuzov@lelypartners.com>
2023-11-17 22:45:58 +0200

    Finished Lesson 4

--- 4_Express_and_Templating_Excercise/services/productSerivce.js
+++ 4_Express_and_Templating_Excercise/services/productSerivce.js
@@ -2,12 +2,13 @@ const Cube = require('../models/cube');
 // ? Done with callback
 // const fs = require('fs');
 // ? Doe with promise
-const fs = require('fs/promises');
-const products = require('../config/products.json');
-const path = require('path');
+// const fs = require('fs/promises');
+// const products = require('../config/products.json');
+// const path = require('path');
+const productData = require('../data/productsData');
 
 function getAll(query) {
-  let result = products;
+  let result = productData.getAll();
 
   if (query.search) {
     result = result.filter((x) => x.name.toLowerCase().includes(query.search.toLowerCase()));
@@ -25,19 +26,21 @@ function getAll(query) {
 }
 
 function getById(id) {
-  return products.find((product) => product.id === id);
+  return productData.getById(id);
 }
 function createProduct(data, cubeId, callback) {
   // ? Done with callback
   // function createProduct(data, cubeId) {
   const cube = new Cube(cubeId, data.name, data.description, data.imageUrl, data.difficultyLevel);
-  products.push(cube);
+  // products.push(cube);
 
   // ? Implemented with callback
   // fs.writeFile(path.join(__dirname, '../config/products.json'), JSON.stringify(products), callback);
 
   // ? Implemented with promise
-  return fs.writeFile(path.join(__dirname, '../config/products.json'), JSON.stringify(products));
+  // return fs.writeFile(path.join(__dirname, '../config/products.json'), JSON.stringify(products));
+
+  return productData.create(cube);
 }
 
 module.exports = { create: createProduct, getAll, getById };
