1b5f088f65fb7652267f87ae59055fbda7881d14
mtopuzov <mtopuzov@lelypartners.com>
2024-01-22 22:30:26 +0200

    Removed REST excercise - bad examples

--- 10_REST_API_Workshop/spa/js/controllers/home.js
+++
@@ -1,8 +0,0 @@
-export default async function home() {
-  this.partials = {
-    header: await this.load('./templates/common/header.hbs'),
-    footer: await this.load('./templates/common/footer.hbs')
-  };
-
-  this.partial('./templates/home.hbs', this.app.userData);
-}
